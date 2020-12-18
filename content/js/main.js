9afe0eea825bee55254d58972292af98b68d7283
Libor Krzyzanek <libor@krzyzanek.com>
2020-12-18 12:19:23 +0100

    simple rest api test

--- content/js/main.js
+++ content/js/main.js
@@ -1,1 +1,17 @@
 console.log('test');
+
+function callApi(url) {
+    console.log('calling rest api: ' + url);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            document.getElementById("restapi-call-result").innerHTML = this.responseText;
+        }
+
+    };
+    xhr.onerror = function (err) {
+        updateResult(err);
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
